bdf2a5d7cdd4076449b2ee3a91d7b9793e8e3638
Gideon Aiyan <aiyan8212002@gmail.com>
2023-12-08 19:25:23 +0100

    users added

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from mainapp import views
 
 urlpatterns=[
-    path('', views.home, name="Home"),
-    path('map', views.interactive_map, name="Map"),
-    path('route', views.route, name="Route"),
-    path('bicimad_status', views.bicimad_status, name="Bicimad_status"),
-    path('bicimad_ui', views.bicimad_ui, name="Bicimad_ui")
+    path('', views.home, name="home"),
+    path('register',views.register,name="register"),
+    path('login',views.login_page,name="login"),
+    path('logout',views.logout_user,name="logout"),
+    path('map', views.interactive_map, name="map"),
+    path('route', views.route, name="route"),
+    path('bicimad_status', views.bicimad_status, name="bicimad_status"),
+    path('bicimad_ui', views.bicimad_ui, name="bicimad_ui")
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib import messages
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+from .forms import CreateUserForm
+
+def login_page(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+    context={}
+    return render(request,'mainapp/login.html',context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method=='POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for '+ user)
+            return redirect('login')
+    context = {'form':form}
+    return render(request,'mainapp/register.html',context)
+
 
 def home(request):
     return render(request,'mainapp/home.html')
@@ -14,6 +49,10 @@ def route(request):
 def bicimad_status(request):
     return HttpResponse("bicimad")
 
+
+@login_required(login_url='login')
 def bicimad_ui(request):
     return HttpResponse("bicimad_ui")
+
+
 
