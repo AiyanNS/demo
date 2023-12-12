95110c969565b3aaafeff13e3afdda91437d85dd
Gideon Aiyan <aiyan8212002@gmail.com>
2023-12-12 20:33:20 +0100

    csrf

--- iRoute/settings.py
+++ iRoute/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     'mainapp'
 ]
 
@@ -117,13 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/mainapp/'
-
-MEDIA_URL='/images/'
-
-STATICFILES_DIR=[
-    os.path.join(BASE_DIR,'static/mainapp/')
-]
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -133,6 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = False
-
 CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = ['http://iroutedemo.azurewebsites.net']
