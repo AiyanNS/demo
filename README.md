1c1330532c2c21b085fc0bb9e065ef201c3572e3
Gideon Aiyan <aiyan8212002@gmail.com>
2023-12-17 21:55:57 +0100

    readme.md added

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# demo
-iroute
+# iRoute proof of concept
+
+## Overview
+This repository contains the source code for a iRoute. The system aims to provide route planning, real-time tracking, and other useful features for users relying on public transportation. The project is built using Django, incorporating machine learning models for prediction and optimization.
+
+## Getting Started
+
+### Prerequisites
+- Ensure you have Python installed on your system.
+- Install required Python packages: `pip install -r requirements.txt`
+
+### Running the Application
+1. Navigate to the project directory.
+2. Run the Django development server: `python manage.py runserver`
+3. Access the application through the provided local address (usually `http://127.0.0.1:8000/`).
+
+### Understanding manage.py
+The `manage.py` script is a command-line utility provided by Django for various tasks, including running the development server, applying database migrations, and creating administrative users. For example, to start the development server, you can use the command: `python manage.py runserver`.
+
+### Machine Learning Models
+- `pred.ipynb`: This Jupyter Notebook contains the code for training the three different machine learning models used in the project. It covers the training process for prediction and optimization tasks.
+
+### Dataset Generation
+- `data_gen.py`: This Python script is responsible for generating datasets used during the training of the machine learning models. It ensures the availability of diverse and relevant data for effective model training.
+
+### Additional Files
+- The remaining files in the repository are test data files utilized during the development and testing phases of the project.
+
--- mainapp/models.py
+++ mainapp/models.py
@@ -4,18 +4,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class BicimadStation(models.Model):
-    id = models.AutoField(primary_key=True)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    name = models.CharField(max_length=555)
-    number = models.CharField(max_length=100)
-    address = models.CharField(max_length=555)
-    activate = models.IntegerField()
-    no_available = models.IntegerField()
-    total_bases = models.IntegerField()
-    dock_bikes = models.IntegerField()
-    free_bases = models.IntegerField()
-    reservations_count = models.IntegerField()
+    id                  = models.AutoField(primary_key=True)
+    latitude            = models.FloatField()
+    longitude           = models.FloatField()
+    name                = models.CharField(max_length=555)
+    number              = models.CharField(max_length=100)
+    address             = models.CharField(max_length=555)
+    activate            = models.IntegerField()
+    no_available            = models.IntegerField()
+    total_bases             = models.IntegerField()
+    dock_bikes          = models.IntegerField()
+    free_bases          = models.IntegerField()
+    reservations_count  = models.IntegerField()
 
     def __str__(self):
         return f"{self.name} ({self.number})"
@@ -26,12 +26,12 @@ class savedBICIMAD(models.Model):
 
 
 class Estacion(models.Model):
-    IDESTACION = models.IntegerField(primary_key=True)
-    DENOMINACION = models.CharField(max_length=255)
-    LINEAS = models.CharField(max_length=255)
-    longitude = models.FloatField()
-    latitude = models.FloatField()
-    ACONDICIONAMIENTOVIAJEROS=models.IntegerField()
+    IDESTACION                  = models.IntegerField(primary_key=True)
+    DENOMINACION                = models.CharField(max_length=255)
+    LINEAS                      = models.CharField(max_length=255)
+    longitude                   = models.FloatField()
+    latitude                    = models.FloatField()
+    ACONDICIONAMIENTOVIAJEROS   =models.IntegerField()
     def __str__(self):
         return f"{self.IDESTACION} - {self.DENOMINACION}"
     
